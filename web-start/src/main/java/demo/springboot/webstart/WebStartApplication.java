b10c8650b7bc51fbfee0e89815a065802b112048
dean.lee <raines.lee@aliyun.com>
2019-05-29 14:01:49 +0800

    springboot启动web项目

---
+++ web-start/src/main/java/demo/springboot/webstart/WebStartApplication.java
@@ -0,0 +1,13 @@
+package demo.springboot.webstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStartApplication.class, args);
+    }
+
+}
