13a6a32b923c32900ff59972a79a80ae06e4e372
dean.lee <dean.lee@aliyun.com>
2019-11-11 21:11:11 +0800

    防刷api添加过期时间

--- limiter/src/main/java/demo/springboot/limiter/annotation/Limiter.java
+++ limiter/src/main/java/demo/springboot/limiter/annotation/Limiter.java
@@ -15,20 +15,31 @@ import java.lang.annotation.Target;
 public @interface Limiter {
 
     /**
-     * 从第一次访问接口的时间到cycle周期时间内，无法超过frequency次，默认60次
+     * 从第一次访问接口的时间到cycle周期时间内，无法超过frequency次
+     *
      * @return
      */
-    int frequency() default 60;
+    int frequency() default 20;
 
     /**
-     * 周期时间,默认30分钟内
+     * 周期时间,单位ms：
+     * 默认周期时间为一分钟
+     *
      * @return
      */
-    int cycle() default 30 * 60 * 1000;
+    long cycle() default 60 * 1000;
 
     /**
      * 返回的错误信息
+     *
      * @return
      */
     String message() default "请求过于频繁";
+
+    /**
+     * 到期时间,单位s：
+     * 如果在cycle周期时间内超过frequency次，则默认1分钟内无法继续访问
+     * @return
+     */
+    long expireTime() default 1 * 60;
 }
--- limiter/src/main/java/demo/springboot/limiter/aspect/LimitingAspect.java
+++ limiter/src/main/java/demo/springboot/limiter/aspect/LimitingAspect.java
@@ -14,59 +14,60 @@ import org.springframework.stereotype.Component;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @author dean.lee
+ * ip防刷api功能实现。
+ * 该功能使用redis作为存储，方便在集群中使用。
+ * 如果是单项目部署，可以将redis换成本地缓存。
  *
- * aop操作日志记录
+ * @author dean.lee
+ * <p>
  */
 @Aspect
 @Component
 public class LimitingAspect {
-    //redis中存储的key
-    private static final String LIMITER_KEY = "limiter:%s:%s";
-    private static final String LIMITER_BEGINTIME = "beginTime";
-    private static final String LIMITER_EXFREQUENCY = "exFrequency";
+    private static final String LIMITING_KEY = "limiting:%s:%s";
+    private static final String LIMITING_BEGINTIME = "beginTime";
+    private static final String LIMITING_EXFREQUENCY = "exFrequency";
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
     @Pointcut("@annotation(limiter)")
-    public void pointcut(Limiter limiter) {}
+    public void pointcut(Limiter limiter) {
+    }
 
     @Around("pointcut(limiter)")
     public Object around(ProceedingJoinPoint pjp, Limiter limiter) throws Throwable {
-        //获取请求的ip和访问方法的名称
+        //获取请求的ip和方法
         String ipAddress = WebUtil.getIpAddress();
         String methodName = pjp.getSignature().toLongString();
 
         //获取方法的访问周期和频率
         long cycle = limiter.cycle();
         int frequency = limiter.frequency();
-        //获取访问方法的时间
         long currentTime = System.currentTimeMillis();
 
         //获取redis中周期内第一次访问方法的时间和执行的次数
-        Long beginTime = (Long) redisTemplate.opsForHash().get(String.format(LIMITER_KEY, ipAddress, methodName), LIMITER_BEGINTIME);
-        Integer exFrequency = (Integer) redisTemplate.opsForHash().get(String.format(LIMITER_KEY, ipAddress, methodName), LIMITER_EXFREQUENCY);
+        Long beginTimeLong = (Long) redisTemplate.opsForHash().get(String.format(LIMITING_KEY, ipAddress, methodName), LIMITING_BEGINTIME);
+        Integer exFrequencyLong = (Integer) redisTemplate.opsForHash().get(String.format(LIMITING_KEY, ipAddress, methodName), LIMITING_EXFREQUENCY);
 
-        beginTime = beginTime == null ? 0 : beginTime;
-        exFrequency = exFrequency == null ? 0 : exFrequency;
+        long beginTime = beginTimeLong == null ? 0L : beginTimeLong;
+        int exFrequency = exFrequencyLong == null ? 0 : exFrequencyLong;
 
         //如果当前时间减去周期内第一次访问方法的时间大于周期时间，则正常访问
         //并将周期内第一次访问方法的时间和执行次数初始化
-        if(currentTime - beginTime > cycle){
-            redisTemplate.opsForHash().put(String.format(LIMITER_KEY, ipAddress, methodName), LIMITER_BEGINTIME, currentTime);
-            redisTemplate.opsForHash().put(String.format(LIMITER_KEY, ipAddress, methodName), LIMITER_EXFREQUENCY, 1);
-            //设置过期时间
-            redisTemplate.expire(String.format(LIMITER_KEY, ipAddress, methodName), limiter.cycle(), TimeUnit.MILLISECONDS);
+        if (currentTime - beginTime > cycle) {
+            redisTemplate.opsForHash().put(String.format(LIMITING_KEY, ipAddress, methodName), LIMITING_BEGINTIME, currentTime);
+            redisTemplate.opsForHash().put(String.format(LIMITING_KEY, ipAddress, methodName), LIMITING_EXFREQUENCY, 1);
+            redisTemplate.expire(String.format(LIMITING_KEY, ipAddress, methodName), limiter.expireTime(), TimeUnit.SECONDS);
             return pjp.proceed();
-        }else{
+        } else {
             //如果在周期时间内，执行次数小于频率，则正常访问
             //并将执行次数加一
-            if(exFrequency < frequency){
-                redisTemplate.opsForHash().put(String.format(LIMITER_KEY, ipAddress, methodName), LIMITER_EXFREQUENCY, exFrequency + 1);
-                redisTemplate.expire(String.format(LIMITER_KEY, ipAddress, methodName), limiter.cycle(), TimeUnit.MILLISECONDS);
+            if (exFrequency < frequency) {
+                redisTemplate.opsForHash().put(String.format(LIMITING_KEY, ipAddress, methodName), LIMITING_EXFREQUENCY, exFrequency + 1);
+                redisTemplate.expire(String.format(LIMITING_KEY, ipAddress, methodName), limiter.expireTime(), TimeUnit.SECONDS);
                 return pjp.proceed();
-            }else {
+            } else {
                 //否则抛出访问频繁异常
                 throw new FrequentRequestsException(limiter.message());
             }
